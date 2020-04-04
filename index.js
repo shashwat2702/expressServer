e9b00c6fb8834557219ec96a6e5ac8ac481d5a0a
Shashwat Sinha <shashwat-sinha@VIMID-SinhaS-LM.local>
2020-04-05 00:25:52 +0530

    Setup body parser and jwt

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const userName = req.query.username;
+  console.log(userName);
+  const token = jwt.sign({ foo: "bar" }, "shhhhh");
+  res.json({ token, userName });
+});
+
+app.post("/", (req, res) => res.send("Got a POST request"));
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Got a DELETE request at /user");
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
