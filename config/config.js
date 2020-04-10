1ce8dee682b566cb231c4729f298b21ff12753af
Shashwat Sinha <shashwat-sinha@VIMID-SinhaS-LM.local>
2020-04-10 18:02:25 +0530

    Made changes for deployment

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "express_database_development",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+  test: {
+    username: "postgres",
+    password: null,
+    database: "express_database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+  production: {
+    username: process.env.DATABASE_URL.split(":")[1].split("/")[2],
+    password: process.env.DATABASE_URL.split(":")[2].split("@")[0],
+    database: process.env.DATABASE_URL.split(":")[3].split("/")[1],
+    host: process.env.DATABASE_URL.split(":")[2].split("@")[1],
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+};
