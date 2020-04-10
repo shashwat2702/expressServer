f84b776f2b0b153e89ba435d0bb50d487ca800b7
Shashwat Sinha <shashwat-sinha@VIMID-SinhaS-LM.local>
2020-04-10 14:54:43 +0530

    Created model and  added functions to it

---
+++ models/user.js
@@ -0,0 +1,51 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    "user",
+    {
+      firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {}
+  );
+
+  user.registerUser = ({
+    inputEmail,
+    inputFirstName,
+    inputLastName,
+    inputPassword,
+  }) =>
+    user
+      .create({
+        email: inputEmail,
+        firstName: inputFirstName,
+        lastName: inputLastName,
+        password: inputPassword,
+      })
+      .then((response) => response);
+
+  user.isEmailTaken = (inputEmail) =>
+    user.findOne({ where: { email: inputEmail } }).then((response) => response);
+
+  user.checkCredential = (inputEmail, inputPassword) =>
+    user
+      .findOne({
+        where: { email: inputEmail, password: inputPassword },
+      })
+      .then((response) => response);
+  return user;
+};
